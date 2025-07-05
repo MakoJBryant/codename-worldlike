8107ca1f4591ca41db1336e590ae87c890ac91c0
Mako J. Bryant <mako.j.bryant@outlook.com>
2025-07-05 16:40:24 -0400

    🚀 Initial project setup with basic structure

--- Assets/Celestial/Generation/PlanetSettings.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PlanetSettings", menuName = "Planet/PlanetSettings")]
-public class PlanetSettings : ScriptableObject
-{
-    public ShapeSettings shape;
-    public ShadingSettings shading;
-}
